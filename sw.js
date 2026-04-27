5200614828f3b486e58a3bdb0164255941ff9a23
CKJ5 <chengkohjun@gmail.com>
2026-04-27 10:56:39 +0800

    Update sw.js
    owlf-v3

--- sw.js
+++ sw.js
@@ -1,30 +1,28 @@
-const CACHE = 'owlf-v2';
+const CACHE = 'owlf-v3';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
   e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll([
-      './',
-      './index.html'
-    ]).catch(() => {}))
+    caches.open(CACHE).then(c => {
+      return c.add('./index.html');
+    })
   );
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
-    )).then(() => clients.claim())
-  );
+  self.skipWaiting();
+  e.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', e => {
+  if (e.request.mode === 'navigate') {
+    e.respondWith(
+      fetch(e.request).catch(() => caches.match('./index.html'))
+    );
+    return;
+  }
   e.respondWith(
-    fetch(e.request).then(res => {
-      const clone = res.clone();
-      caches.open(CACHE).then(c => c.put(e.request, clone));
-      return res;
-    }).catch(() => caches.match(e.request))
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
 
